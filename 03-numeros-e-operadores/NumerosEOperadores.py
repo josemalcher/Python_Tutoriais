29819794a1cb77bc1d1fc19193df170532152ff5
José Malcher Junior <contato@josemalcher.net>
2017-02-24 15:10:53 -0300

    Números e Operadores no Python 3

---
+++ 03-numeros-e-operadores/NumerosEOperadores.py
@@ -0,0 +1,42 @@
+#3.1.1 Números e Operadores no Python 3 (parte 1)
+
+#5+5 -> sempre devolve um inteiro
+print(5+5)
+#5 * 5
+print(5+5)
+#5 - 5
+print(5+5)
+#5 /5 -> devolve um tipo float
+print(5/5)
+
+print(5/6)
+
+# segue () para dar prioridade.
+print((10+2)/5 )
+
+
+#3.1.1 Números e Operadores no Python 3 (parte 2)
+
+#para obter um inteiro ( Não arrendonda)
+print( 10//10 )
+
+#Potencia
+print( 5 ** 2 )
+
+#Resto da divição
+print(17 % 3)
+
+#variáveis
+abel = 100;
+print(abel)
+
+#no terminal - O " _ " pega o ultimo valor do resultado no terminarl
+
+# 5 + 10 * 2.5
+# 30.0
+# 20 + _
+# 50.0
+
+# arredondamento
+print(round(15.6666666666, 2))
+print(round(17/3, 2))
