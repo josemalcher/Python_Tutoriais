a9ac0da37168b004199a8fefafebf422498889b8
José Malcher Junior <contato@josemalcher.net>
2017-03-29 14:36:51 -0300

    3.2.4 Variável de controle - Python 3.5 Tutorial

---
+++ 03-numeros-e-operadores/aula3-2-4-VariavelControle.py
@@ -0,0 +1,13 @@
+# 3.2.4 Variável de controle - Python 3.5 Tutorial
+# https://www.youtube.com/watch?v=53vAnyYFC3g
+
+a = 1
+while a < 5:
+    print(a)
+    a = a + 1
+print("###################")
+
+a, b = 0, 1
+while a<10:
+    print(b)
+    a,b = b, a+b
