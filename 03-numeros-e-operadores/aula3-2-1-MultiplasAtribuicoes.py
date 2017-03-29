43c30dd53aaeef819d761334ee9d156d3325303f
José Malcher Junior <contato@josemalcher.net>
2017-03-29 14:05:38 -0300

    3.2.1 Múltiplas Atribuições

---
+++ 03-numeros-e-operadores/aula3-2-1-MultiplasAtribuicoes.py
@@ -0,0 +1,14 @@
+# Abel Separovich
+# 3.2.1 Múltiplas Atribuições
+# https://www.youtube.com/watch?v=T37_w-FZg0E
+
+a = 1
+b = 2
+c = 3
+
+print(a,b,c)
+
+a,b,c = 4,5,6
+print(a,b,c)
+
+#no modo interativo (4,5,6)  formamos uma tupla de 3 elementos
