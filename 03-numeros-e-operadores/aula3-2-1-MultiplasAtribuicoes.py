3d1166b598b96895e5c17a4d068bb6cc6ba2b19f
José Malcher Junior <contato@josemalcher.net>
2017-03-29 14:11:36 -0300

    3.2.2 Atribuição e Fibonacci (Correção e adendo)

--- 03-numeros-e-operadores/aula3-2-1-MultiplasAtribuicoes.py
+++ 03-numeros-e-operadores/aula3-2-1-MultiplasAtribuicoes.py
@@ -1,5 +1,6 @@
 # Abel Separovich
 # 3.2.1 Múltiplas Atribuições
+# 3.2.2 Atribuição e Fibonacci (Correção e adendo)
 # https://www.youtube.com/watch?v=T37_w-FZg0E
 
 a = 1
@@ -11,4 +12,18 @@ print(a,b,c)
 a,b,c = 4,5,6
 print(a,b,c)
 
-#no modo interativo (4,5,6)  formamos uma tupla de 3 elementos
+#no modo interativo (4,5,6) 3 tuplas
+
+a, b = 0, 1
+a, b = b, a + b
+print(a)
+print(b)
+
+a, b = b, a + b
+print(a,b)
+
+a, b = b, a + b
+print(a,b)
+
+a, b = b, a + b
+print(a,b)
