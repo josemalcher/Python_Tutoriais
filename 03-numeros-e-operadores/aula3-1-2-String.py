d8cea6cf1133b8eee14dae616f7caeb102f702e3
José Malcher Junior <contato@josemalcher.net>
2017-03-29 11:58:20 -0300

    3.1.2 Strings (Parte 1 até 6) Python 3.5 Tutorial

---
+++ 03-numeros-e-operadores/aula3-1-2-String.py
@@ -0,0 +1,68 @@
+# 3.1.2 Strings (Parte 1) Python 3.5 Tutorial
+# 3.1.2 Strings (parte 2) Python 3.5 Tutorial
+# 3.1.2 Strings (Parte 3) Python 3.5 Tutorial
+# 3.1.2 Strings (Parte 4) Python 3.5 Tutorial
+# 3.1.2 Strings (Parte 5) Python 3.5 Tutorial
+# 3.1.2 Strings (Parte 6) Python 3.5 Tutorial
+# https://www.youtube.com/watch?v=KoHTcslZlHY&t=2s
+#
+
+# Usar aspas '' ou " "
+# " Don't eat"
+# 'A pessoa falou "tal tal tal" '
+# 'Don\'t eat'
+# "A pessoa falou \"tal tal tal\" "
+
+
+print('teste')
+print("Teste")
+print("A pessoa falou \"tal tal tal\" ")
+print('Don\'t eat')
+
+# Imprimir sem pular
+print('c:\meupc\nome')
+print(r'c:\meupc\nome')
+print(r'c:\meupc\nome\\')
+
+# pular de linha
+print('Primeira linha \n Segunda linha \n Terceira LIha')
+
+# Texto pré formatado
+print('''' teste te llinhas      
+ espaços e pulos
+ teste''''' )
+
+#Concatenação
+print("teste " + "  PASSOU")
+
+# Concatenação terminal
+# 5 * 'tic '
+print(5 * 'tic ' + 'tac')
+
+# Uso de Variável
+variavel = 'JOSE'
+print(variavel)
+
+n = 'Jose ' + 'Malcher'
+print(n)
+
+# Guardar a Primeira Letra ou mais letras
+variavel01 = 'JOSE'
+print(variavel[0] + variavel[1])
+
+#ultima letra
+texto = 'Texto longo para'
+print(texto[-1])
+
+# Parte de um texto
+print(texto[0:8])
+print(texto[4:])
+print(texto[:8])
+
+# quantidade de caracteres
+print(len(texto))
+
+
+
+
+
