2bae48f34f892b58247baf29968f5ddfb1807f8c
José Malcher Junior <contato@josemalcher.net>
2017-03-29 17:29:26 -0300

    4.2 For - Tutorial de Python 3 em Portugues

---
+++ 04-Controle-e-fluxos/aula4-2-For.py
@@ -0,0 +1,12 @@
+# 4.2 For - Tutorial de Python 3 em Portugues
+# https://www.youtube.com/watch?v=y0gVQGnV2K8
+
+Alunos = ['Abel', 'Alvaro', 'Laise','Nitun', 'Welinton','Jose']
+
+for nome in Alunos:
+    print(nome , end=', ')
+
+numeros = [1,2,3,4,5,6,8,7,6,5,4]
+for numero in numeros:
+    print(numero, ' x ', numero , ' = ', numero*numero)
+
