3ff619e257d67e50987cffd6d951045cecf865e3
José Malcher Junior <contato@josemalcher.net>
2017-03-29 14:22:29 -0300

    3.2.3 While e Operadores Lógicos - Python 3.5 Tutorial

---
+++ 03-numeros-e-operadores/aula3-2-3-WhileOperadoresLogicos.py
@@ -0,0 +1,17 @@
+# 3.2.3 While e Operadores Lógicos - Python 3.5 Tutorial
+# https://www.youtube.com/watch?v=an5NQVJNwOM
+#
+#
+
+while False:
+    print("FALSE")
+while True:
+    print("OK, VERDADE")
+    break
+
+i = 10
+while i < 20:
+    print(i)
+    i = i + 1
+
+
