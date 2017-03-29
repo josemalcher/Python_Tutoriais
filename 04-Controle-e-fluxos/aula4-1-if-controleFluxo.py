c5f693150fb89d15648329f98c7b74b6cfd071e5
José Malcher Junior <contato@josemalcher.net>
2017-03-29 16:10:53 -0300

    4.1 If - Controle de Fluxo (Parte 1) - Python 3 Tutorial

---
+++ 04-Controle-e-fluxos/aula4-1-if-controleFluxo.py
@@ -0,0 +1,10 @@
+# 4.1 If - Controle de Fluxo (Parte 1) - Python 3 Tutorial
+# https://www.youtube.com/watch?v=tgKoh3N8ZRk
+
+if True:
+    print("UMA VEZ")
+
+if False:
+    x = 1;
+    c = " BLA BLA BLA"
+    print(x, c)
