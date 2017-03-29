c84d18812758a008bfe94bf4d82116d63d48771f
José Malcher Junior <contato@josemalcher.net>
2017-03-29 18:57:46 -0300

    4.4 Break (Parte 2) - Tutorial de Python 3 Português

---
+++ 04-Controle-e-fluxos/aula4-4-Break.py
@@ -0,0 +1,10 @@
+# 4.4 Break (Parte 2) - Tutorial de Python 3 Português
+# https://www.youtube.com/watch?v=6Spd2MA6rfA
+
+for n in range(10):
+    print(n, end=',')
+    if n == 5:
+        break
+else:
+    print('Final natural.')
+print("Fim do Codigo!!!")
