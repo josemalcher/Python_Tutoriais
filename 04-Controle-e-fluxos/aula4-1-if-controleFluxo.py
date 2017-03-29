d47c87038f17f3e169978deaae467c9c11213beb
José Malcher Junior <contato@josemalcher.net>
2017-03-29 16:52:50 -0300

    4.1 else, elif - Controle de Fluxo (Parte 2) - Python 3 Tutorial

--- 04-Controle-e-fluxos/aula4-1-if-controleFluxo.py
+++ 04-Controle-e-fluxos/aula4-1-if-controleFluxo.py
@@ -1,4 +1,5 @@
 # 4.1 If - Controle de Fluxo (Parte 1) - Python 3 Tutorial
+# 4.1 else, elif - Controle de Fluxo (Parte 2) - Python 3 Tutorial
 # https://www.youtube.com/watch?v=tgKoh3N8ZRk
 
 if True:
@@ -8,3 +9,15 @@ if False:
     x = 1;
     c = " BLA BLA BLA"
     print(x, c)
+else:
+    print("Foi verdaDE!!")
+
+print("\n ###### \n")
+
+a = int(input('fale um numero\n'))
+if a < 2:
+    print("é maior que 2")
+elif a == 2:
+    print("é igual a 2")
+else:
+    print("Isso é menos do que dois")
