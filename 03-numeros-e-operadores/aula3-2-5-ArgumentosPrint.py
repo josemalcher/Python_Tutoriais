b23f2d6e177078201dbbf3d09474fb7629d11c78
José Malcher Junior <contato@josemalcher.net>
2017-03-29 14:43:40 -0300

    3.2.5 Argumentos no Print - Python 3.5 Tutorial

---
+++ 03-numeros-e-operadores/aula3-2-5-ArgumentosPrint.py
@@ -0,0 +1,10 @@
+# 3.2.5 Argumentos no Print - Python 3.5 Tutorial
+# https://www.youtube.com/watch?v=tEpnmlgexWo
+
+print("JOSE", 2 , 3) #quebra linha no final
+print("MALCHER", end=' --- ')
+print("########")
+a = 0
+while a <= 10:
+    print('Itens', end=', ')
+    a = a+1
