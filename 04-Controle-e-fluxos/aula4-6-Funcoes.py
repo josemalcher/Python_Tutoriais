64b30b04d663b6744ee7e2770666f559b7f13570
José Malcher Junior <contato@josemalcher.net>
2017-03-30 04:02:42 -0300

    4.6 def - Funções (Parte 1) - Tutorial Python 3 em Português

---
+++ 04-Controle-e-fluxos/aula4-6-Funcoes.py
@@ -0,0 +1,12 @@
+# 4.6 def - Funções (Parte 1) - Tutorial Python 3 em Português
+# https://www.youtube.com/watch?v=ekHovQkbmVc
+
+def nomeIdade():
+    nome = input('Escreva seu nome: ')
+    idade = int(input('Escreva sua idade: '))
+
+    print(nome, ' tem idade de ', idade, '\n Próximo!! \n')
+
+nomeIdade()
+nomeIdade()
+nomeIdade()
