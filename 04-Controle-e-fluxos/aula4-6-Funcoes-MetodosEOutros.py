7beba9967e7b35a4f82bf725bf85852340bf6b2f
José Malcher Junior <contato@josemalcher.net>
2017-03-30 04:37:38 -0300

    4.6 Métodos e outros - Funções (Parte 4) - Tutorial Python 3 Em Português

---
+++ 04-Controle-e-fluxos/aula4-6-Funcoes-MetodosEOutros.py
@@ -0,0 +1,15 @@
+# 4.6 Métodos e outros - Funções (Parte 4) - Tutorial Python 3 Em Português
+# https://www.youtube.com/watch?v=uKQoVppDOsw
+
+def exemplo(num):
+    """ Imprime exemplo mais o numeor de argumentos (DOC da Função)"""
+    print("Exemplo", num)
+
+e = exemplo # forma abreviada de chamar as funções
+e(2)
+
+
+a = []
+a.append(1) # append é um método
+print(a)
+
