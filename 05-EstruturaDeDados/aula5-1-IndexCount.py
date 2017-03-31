c20e091af56e3fcf02bffd928515341d8a8cf8a0
José Malcher Junior <contato@josemalcher.net>
2017-03-31 14:15:11 -0300

    5.1 index() e count() (Parte 2) - Tutorial de Python 3 em português :snake:

---
+++ 05-EstruturaDeDados/aula5-1-IndexCount.py
@@ -0,0 +1,20 @@
+# 5.1 index() e count() (Parte 2) - Tutorial de Python 3 em português
+# https://www.youtube.com/watch?v=a8ovCF8nr9U
+
+
+a = ['agua','pera', 'maca' ,'banana','só','pera']
+print(a.index('maca'))
+
+#lista.index(valor, inicio, fim)
+print(a.index('banana',2,4))
+
+#dá erro quando não encontrado
+# a.index('so')
+
+#contas as ocorrencias
+print(a.count('pera'))
+
+
+
+
+
