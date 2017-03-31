37bcdd96b1c07c6cfd0a53b9d1de83af680f8ad8
José Malcher Junior <contato@josemalcher.net>
2017-03-31 14:55:30 -0300

    5.1.1 Lista como Pilhas - Estrutura de dados - Tutorial de Python 3 em Português

---
+++ 05-EstruturaDeDados/aula5-1-1-ListaComoPilha.py
@@ -0,0 +1,18 @@
+# 5.1.1 Lista como Pilhas - Estrutura de dados - Tutorial de Python 3 em Português
+# https://www.youtube.com/watch?v=YVKWaS0NsXU
+
+
+livros = []
+
+livros.append('Livro 1')
+livros.append('Livro 2')
+livros.append('Livro 3')
+print(livros)
+
+livros.pop()
+print(livros)
+
+livros.append('Livros x')
+print(livros)
+
+
