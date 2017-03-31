8a56f6e7a8a8fedba2acc7ba9fba63f1017f2cca
José Malcher Junior <contato@josemalcher.net>
2017-03-31 06:49:09 -0300

    4.7.1 Definindo Variáveis - Funções (Parte 1) - Python Tutorial em Portugues

---
+++ 04-Controle-e-fluxos/aula4-7-Funcoes-DefinindoVariaveis.py
@@ -0,0 +1,11 @@
+# 4.7.1 Definindo Variáveis - Funções (Parte 1) - Python Tutorial em Portugues
+# https://www.youtube.com/watch?v=Vt2eXtXWY3k
+
+def ex(n1,n2,n3):
+    print(n1)
+    print(n2)
+    print(n3)
+
+ex(10,9,8)
+
+ex(n2=10, n3=9, n1=8)
