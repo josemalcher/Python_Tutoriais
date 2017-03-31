5379cb9bb99f94c2297e90b27c6871562785f45a
José Malcher Junior <contato@josemalcher.net>
2017-03-31 11:37:49 -0300

    4.7.5 Expressão lambda - Tutorial de Python 3 em português

---
+++ 04-Controle-e-fluxos/aula4-7-5-ExpLambda.py
@@ -0,0 +1,18 @@
+# 4.7.5 Expressão lambda - Tutorial de Python 3 em português
+# https://www.youtube.com/watch?v=t9-w8iekj1o
+
+def dob(a):
+    return a * 2
+
+print(dob(10))
+
+d = dob
+print(d(30))
+
+# nome = lambda argumentos: retorno
+dobro = lambda b: b*2
+print(dobro(50))
+
+
+
+
