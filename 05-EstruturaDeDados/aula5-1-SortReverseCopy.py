a30badc0cc0a61d2ef0c457562ccd4052f315808
José Malcher Junior <contato@josemalcher.net>
2017-03-31 14:31:55 -0300

    5.1 sort(), reverse() e copy() (Parte 3)- Tutorial Python em Portugues

---
+++ 05-EstruturaDeDados/aula5-1-SortReverseCopy.py
@@ -0,0 +1,32 @@
+# 5.1 sort(), reverse() e copy() (Parte 3)- Tutorial Python em Portugues
+# https://www.youtube.com/watch?v=B7zrMliGQO0
+
+
+lista = [1,2,3,4,5,6,7]
+print(lista)
+lista.reverse()
+print(lista)
+
+frutas = ['laranja','pera','maçã','abacaxi', 'mamão']
+print(frutas)
+frutas.sort()
+print(frutas)
+
+frutas.copy()
+
+#ou
+frutas[:]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
