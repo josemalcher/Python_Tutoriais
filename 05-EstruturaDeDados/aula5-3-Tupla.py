d387585762bb648f1259a1912ba0b285813e2a27
José Malcher Junior <contato@josemalcher.net>
2017-04-06 16:59:27 -0300

    5.3 Tuplas - Tutorial de Python 3 em Português

---
+++ 05-EstruturaDeDados/aula5-3-Tupla.py
@@ -0,0 +1,27 @@
+# 5.3 Tuplas - Tutorial de Python 3 em Português
+# https://www.youtube.com/watch?v=CCgGXg_gMS0
+
+lista = [12,2,3,4,4] #mutável
+tupla = (1,2,3,4,5)  #imutável
+tupla2 = 1,2,3,4
+
+tuplaVazia = ()
+
+# guardar uma tupla
+# var = 1,
+
+#guardar um objeto mutavel dentro de um objeto imutável
+
+varTupla = lista, tupla, tuplaVazia
+
+print(varTupla)
+
+print(tupla2)
+
+a = 1,2,3,4, "1","2","3","4"
+print(a)
+
+a = ((1,2,3,4), ("1","2","3","4"))
+print(a)
+
+
