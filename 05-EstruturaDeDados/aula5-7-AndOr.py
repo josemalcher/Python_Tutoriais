dee4916b8b928d52f53469319e543a73d15ee0f5
José Malcher Junior <contato@josemalcher.net>
2017-04-21 18:40:14 -0300

    5.7 AND e OR - Comparadores - Tutorial de Python 3 em Português

---
+++ 05-EstruturaDeDados/aula5-7-AndOr.py
@@ -0,0 +1,24 @@
+# 5.7 AND e OR - Comparadores - Tutorial de Python 3 em Português
+# https://www.youtube.com/watch?v=nyMe5gvHmsg
+
+
+
+"""=========== AND ==========="""
+
+print(True and True and True and True)
+# True
+print(((True and True)and True)and True)
+# True
+
+print(True and True and False and True)
+# False
+
+"""=========== OR ==========="""
+print(True or True or True or True)
+# TRUE
+
+print(False or True or True or True)
+# TRUE
+
+print(False or False or False or False)
+# False
