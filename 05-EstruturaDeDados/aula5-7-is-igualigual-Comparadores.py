553c582b7454854dc1bd2970cf7e4bed472f89c9
José Malcher Junior <contato@josemalcher.net>
2017-04-25 16:46:08 -0300

    5.7 is e == - Comparadores - Tutorial de Python 3 em Português

---
+++ 05-EstruturaDeDados/aula5-7-is-igualigual-Comparadores.py
@@ -0,0 +1,18 @@
+# 5.7 is e == - Comparadores - Tutorial de Python 3 em Português
+# https://www.youtube.com/watch?v=ExWh6XhCPLI
+
+# compara os valores
+print(1 == 1)
+# True
+
+# is compara os Objetos, além do valor
+print(1 is 1)
+# True
+
+print(1 == 1.0)
+# True
+
+print(1 is 1.0)
+# False
+
+
