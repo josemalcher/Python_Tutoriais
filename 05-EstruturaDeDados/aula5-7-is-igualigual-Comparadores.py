cf4350e14ac2215170763378db07037fed99e5c8
José Malcher Junior <contato@josemalcher.net>
2017-04-28 15:55:16 -0300

    5.7 is e == - Comparadores - Tutorial de Python 3 em Português - Vídeo com correção sobre a aula!

--- 05-EstruturaDeDados/aula5-7-is-igualigual-Comparadores.py
+++ 05-EstruturaDeDados/aula5-7-is-igualigual-Comparadores.py
@@ -14,5 +14,21 @@ print(1 == 1.0)
 
 print(1 is 1.0)
 # False
+print("######")
+
+# https://www.youtube.com/watch?v=DNpD_TPo0eM
+
+lista01 = ['gato']
+lista02 = lista01
+print(lista01 == lista02) # True
+print(lista01 is lista02) # True
+
+lista03 = ['gato']
+print(lista01 == lista03) # True
+print(lista01 is lista03) # False
+
+
+
+
 
 
